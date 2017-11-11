855b0a05b1cfe5951c984461552d5c542b29bcf3
Rogério da Rocha Rodrigues <rogeriorodrigues@outlook.com>
2017-11-11 15:54:44 -0200

    push no evento judiai dev

---
+++ VisionRecognition/jundev/jundev/jundevPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using IBM.WatsonDeveloperCloud.VisualRecognition.v3;
+using IBM.WatsonDeveloperCloud.VisualRecognition.v3.Model;
+using Plugin.Media;
+using Xamarin.Forms;
+
+namespace jundev
+{
+    public partial class jundevPage : ContentPage
+    {
+
+        string imagem = "https://i.ytimg.com/vi/FAciZRkOKQs/maxresdefault.jpg";
+        public jundevPage()
+        {
+            InitializeComponent();
+
+            SelectedImage.Source = imagem;
+        }
+
+
+        VisualRecognitionService _visualRecognition = new VisualRecognitionService();
+
+       
+        public void GetImageDescription()
+        {
+            
+
+            _visualRecognition.SetCredential("e73ace2dc2f0bed16c7fd0d3175237ce88f9fba9");
+
+            var result =  _visualRecognition.Classify(imagem);
+
+            TextInfo.Text = result.Images[0].Classifiers[0].Name;
+
+            foreach (ClassResult tags in result.Images[0].Classifiers[0].Classes)
+            {
+                InfoLabel.Text += tags._Class + ", ";
+            }
+
+         
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            GetImageDescription();
+        }
+    }
+}
